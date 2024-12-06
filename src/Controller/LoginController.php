211ff8bc8b394d97a6e4a8b9b3f0b81c21e55337
Marek Sterzik <sterzik@spsostrov.cz>
2024-12-06 10:49:30 +0100

    added sso login functionality

---
+++ src/Controller/LoginController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class LoginController extends AbstractController
+{
+    #[Route("/login", name: "login")]
+    public function login(Request $request): Response
+    {
+	return $this->getRedirectResponse($request);
+    }
+
+    #[Route("/logout", name: "logout")]
+    public function logout(Request $request): Response
+    {
+	return $this->getRedirectResponse($request);
+    }
+
+    private function getRedirectResponse(Request $request): Response
+    {
+        $url = $request->query->get("back");
+	if (!is_string($url)) {
+	    return $this->redirectToRoute('main');
+	}
+	return $this->redirect($url);
+    }
+}
