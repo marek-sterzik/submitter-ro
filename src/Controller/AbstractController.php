d8941c5f83c6c4e157100d51c7662631fdc7b0bb
Marek Sterzik <sterzik@spsostrov.cz>
2024-12-06 11:28:14 +0100

    basic nav bar

--- src/Controller/AbstractController.php
+++ src/Controller/AbstractController.php
@@ -10,4 +10,25 @@ use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class AbstractController extends AbstractControllerBase
 {
+    protected function getDefaultParameters()
+    {
+        return [
+            "user" => $this->getUser(),
+        ];
+    }
+
+    protected function renderView(string $view, array $parameters = []): string
+    {
+        $parameters = array_merge($this->getDefaultParameters(), $parameters);
+        return parent::renderView($view, $parameters);
+    }
+
+    protected function render(
+        string $view,
+        array $parameters = [],
+        ?Response $response = null
+    ): Response {
+        $parameters = array_merge($this->getDefaultParameters(), $parameters);
+        return parent::render($view, $parameters, $response);        
+    }
 }
